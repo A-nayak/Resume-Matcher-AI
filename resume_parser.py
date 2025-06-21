9a4b77313af072e83fd917e5de77a1d8f62bd484
A-nayak <107915051+A-nayak@users.noreply.github.com>
2025-06-21 13:48:32 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -3,12 +3,14 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import re
 import spacy
+import spacy
 try:
     nlp = spacy.load("en_core_web_sm")
 except OSError:
     from spacy.cli import download
     download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
+
 
 # Load spaCy model
 nlp = spacy.load("en_core_web_sm")
