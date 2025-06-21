e5b3d815c01c386763430b765ea48a7104212e2e
A-nayak <107915051+A-nayak@users.noreply.github.com>
2025-06-21 14:05:40 +0530

    Update skill_suggester.py

--- skill_suggester.py
+++ skill_suggester.py
@@ -1,16 +1,15 @@
+
 import nltk
 from rake_nltk import Rake
-# Import the function to get the spaCy model from resume_parser
-from resume_parser import get_spacy_model
 
 def extract_keywords_rake(text):
     r = Rake()
     r.extract_keywords_from_text(text)
     return r.get_ranked_phrases()
 
-def extract_keywords_spacy(text):
-    # Get the spaCy model using the getter function
-    nlp_model = get_spacy_model()
+def extract_keywords_spacy(text, nlp_model):
     doc = nlp_model(text)
     keywords = [ent.text for ent in doc.ents if ent.label_ in ["ORG", "GPE", "PRODUCT", "LOC", "PERSON"]]
+    # You can refine this to include specific POS tags as well, e.g., nouns, adjectives
     return list(set(keywords))
+
