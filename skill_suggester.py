43bbf5b8236f6396fe69d317d7696fb01cbc0e18
A-nayak <107915051+A-nayak@users.noreply.github.com>
2025-06-21 14:24:39 +0530

    Update skill_suggester.py

--- skill_suggester.py
+++ skill_suggester.py
@@ -1,6 +1,5 @@
 import nltk
 from rake_nltk import Rake
-# Import the function to get the spaCy model from resume_parser
 from resume_parser import get_spacy_model
 
 def extract_keywords_rake(text):
@@ -9,9 +8,7 @@ def extract_keywords_rake(text):
     return r.get_ranked_phrases()
 
 def extract_keywords_spacy(text):
-    # Get the spaCy model using the robust getter from resume_parser
     nlp_model = get_spacy_model()
     doc = nlp_model(text)
-    # Extract entities that are likely skills or relevant concepts
     keywords = [ent.text for ent in doc.ents if ent.label_ in ["ORG", "GPE", "PRODUCT", "LOC", "PERSON", "NORP"]]
     return list(set(keywords))
